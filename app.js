5a58e928c87688c4b069fe32479205eb46e852df
Breck Cowan <breckcowan@gmail.com>
2022-05-16 21:48:10 -0500

    buttons added in js dynamically

--- app.js
+++ app.js
@@ -71,39 +71,79 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 35.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn');
+const container = document.querySelector('.btn-container')
 
 //load items
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
+  displayMenuButtons();
 });
 
 //filter items
-filterBtns.forEach(function(btn){
-  btn.addEventListener('click', function(e){
-    console.log(e.currentTarget.dataset.id);
-  });
-});
 
 function displayMenuItems(menuItems){
-let displayMenu = menuItems.map(function (item) {
-  // console.log(item);
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+    
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title}>
+    <div class="item-info">
+    <header>
+    <h4>${item.title}</h4>
+    <h4 class="price">${item.price}</h4>
+    </header>
+    <p class="item-text">${item.desc}</p>
+    </div>
+    </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+  sectionCenter.innerHTML = displayMenu;
+}
 
-  return `<article class="menu-item">
-          <img src=${item.img} class="photo" alt=${item.title}>
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">${item.price}</h4>
-            </header>
-            <p class="item-text">${item.desc}</p>
-          </div>
-        </article>`;
+function displayMenuButtons(){
+
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values;
+  }, ['all']);
+  // console.log(categories);
+  const categoryBtns = categories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+  })
+  .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll('.filter-btn');
+  
+  // console.log(categoryBtns);
+  filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+      // console.log(menuItem.category === category);
+      if (menuItem.category === category){
+        return menuItem;
+      }
+    });
+    // console.log(menuCategory);
+    if(category === 'all'){
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
+  });
 });
-displayMenu = displayMenu.join("");
-// console.log(displayMenu);
-sectionCenter.innerHTML = displayMenu;
 }
